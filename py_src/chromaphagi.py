7fc4f38d032d03a85cb8fad0f83712191c4ed9f2
loydp <54460504+loydp@users.noreply.github.com>
2022-11-13 23:21:18 -0800

    Starting new python version

---
+++ py_src/chromaphagi.py
@@ -0,0 +1,15 @@
+
+
+
+
+def main():
+    # 1. take in.
+    # 2. create petridish
+    # 3. select strain
+    # 4. Run it through, with config
+    # 5. Repeat as needed
+    # 6. Show result
+    return None
+
+if __name__ == '__main__':
+    main()
