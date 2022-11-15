a338eb2e985111b0604d4759fe430e3fc85b54e3
loydp <54460504+loydp@users.noreply.github.com>
2022-11-14 21:35:12 -0800

    Added cv2

--- py_src/chromaphagi.py
+++ py_src/chromaphagi.py
@@ -1,9 +1,17 @@
+import os
+import cv2
 
+relative_input_path = '../input/'
 
-
+def find_input_image_path():
+    return os.listdir(relative_input_path)[0]
 
 def main():
     # 1. take in.
+    res = find_input_image_path()
+    print(res)
+    image = cv2.imread(relative_input_path + res)
+    print(image)
     # 2. create petridish
     # 3. select strain
     # 4. Run it through, with config
