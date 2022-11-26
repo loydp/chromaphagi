875d200c7083c5778f91e4cb2703cd8a5e52be09
loydp <54460504+loydp@users.noreply.github.com>
2022-11-25 20:26:37 -0800

    proper name preserved

--- py_src/chromaphagi.py
+++ py_src/chromaphagi.py
@@ -1,24 +1,23 @@
 import os
 import cv2
 
-relative_input_path = '../input/'
-
-def get_input_image():
-    first_image_name = os.listdir(relative_input_path)[0]
-    image = cv2.imread(relative_input_path + first_image_name)
-    return image, first_image_name
+def get_input_image(config):
+    first_image_name = os.listdir(config['input_path'])[0]
+    image = cv2.imread(config['input_path'] + first_image_name)
+    config['file_name'] = first_image_name
+    return image
 
 def save_image(image, name):
-    output_contents = os.listdir(relative_input_path)
+    output_contents = os.listdir(config['output_path'])
     num = 1
     if name in output_contents:
         suffix = f"_{num}"
         # TODO: put name stuff here
-    cv2.imwrite(f"../output/output.jpg", image)
+    cv2.imwrite(f"{config['output_path']}{config['file_name']}", image)
 
-def main():
+def main(config):
     # 1. take in.
-    image, name = get_input_image()
+    image = get_input_image(config)
     image[::-1, ::-1] = image[:, :]
     # 2. create petridish
     # 3. select strain
@@ -28,13 +27,17 @@ def main():
         #print(image[i:i+1:])
         cv2.imshow("Example", image)
         image[i:i+1:, i:i+1:] = [255, 255, 255]
-        cv2.waitKey(300)
+        cv2.waitKey(30)
     # TODO: run it through
     # 5. Repeat as needed
     # 6. Show result
 
-    save_image(image, name)
+    save_image(image, config)
     return None
 
 if __name__ == '__main__':
-    main()
+    config = {
+        "output_path": '../output/',
+        "input_path": '../input/',
+    }
+    main(config)
