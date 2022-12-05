e42fa2e20240c21ed449acab95f13a3f2db576e0
loydp <54460504+loydp@users.noreply.github.com>
2022-12-04 20:51:04 -0800

    a little extra structure before making the strain

--- py_src/chromaphagi.py
+++ py_src/chromaphagi.py
@@ -1,6 +1,15 @@
 import os
 import cv2
 
+class Milleri:
+    def __init__(self):
+        self.tracker = {(0, 0)}
+
+    def step(self, config, image):
+        
+        
+        return True
+
 def get_input_image(config):
     first_image_name = os.listdir(config['input_path'])[0]
     image = cv2.imread(config['input_path'] + first_image_name)
@@ -11,11 +20,8 @@ def save_image(image, config):
     cv2.imwrite(f"{config['output_path']}{config['file_name']}", image)
 
 def _setup(config):
-    pass
+    config['chromaphagi'] = config['chromaphagi']()
 
-
-def _process(config):
-    return True
 
 def main(config):
     image = get_input_image(config)
@@ -23,20 +29,24 @@ def main(config):
     _setup(config)
 
     cv2.imshow("Example", image)
-    for i in range(50):
-        continue_running = _process(config)
+    for i in range(config['max_runtime']):
+        continue_running = config['chromaphagi'].step(config, image)
         if not continue_running:
             break
         cv2.imshow("Example", image)
         cv2.waitKey(config['speed'])
-
+    
     save_image(image, config)
     return None
 
 if __name__ == '__main__':
+    # dunno where this is supposed to go
+
     config = {
-        "speed": 300,
-        "output_path": '../output/',
-        "input_path": '../input/',
+        "max_runtime" : 1,
+        "speed" : 300,
+        "output_path" : '../output/',
+        "input_path" : '../input/',
+        "chromaphagi" : Milleri
     }
     main(config)
